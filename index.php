c89c9a3c8e43abcb72913bce4c5f79a7b867ddca
melto <marink09@gmail.com>
2018-08-24 12:25:00 -0400

    html update Animate svg css update

--- index.php
+++ index.php
@@ -21,21 +21,22 @@
 	    <div class="container">
 
 	    	<div id="home" class="splash-menu has-text-white-ter is-centered">
-	    		<svg id="cross" viewBox=""></svg>
+	    		<svg id="cross"></svg>
 	    		<div class="has-text-centered">
-	    			<p class="is-size-3">Welcome</p>
+	    			<p id="" class="is-size-3">Welcome</p>
 	    		</div>
 	    		<div class="has-text-centered">
-	    			<p class="is-size-3">Music</p>
+	    			<p id="music" class="is-size-3"><a href="#">Music</a></p>
 	    		</div>
 	    		<div class="has-text-centered">
-	    			<p class="is-size-3">Code</p>
+	    			<p id="code" class="is-size-3"><a href="code.php">Code</a></p>
 	    		</div>
 	    		<div class="has-text-centered">
-	    			<p class="is-size-3">Blog</p>
+	    			<p id="blog" class="is-size-3"><a href="blog.php">Blog</a></p>
 	    		</div>
-	    </div>
+		    </div>
 
+		  </div>
 	  </div>
 	</section>
 	<script defer>
@@ -44,17 +45,53 @@
 			var s = Snap("#cross");
 			var line  = s.line(0,0, $('#home').width(), $('#home').width())
 			var line2  = s.line(0,$('#home').width(), $('#home').width(), 0)
+
 			line.attr({
 				stroke: '#fff',
 				strokeWidth: 3,
 				strokeLinecal: 'round'
 			})
+			
 			line2.attr({
 				stroke: '#fff',
 				strokeWidth: 3,
 				strokeLinecal: 'round'
 			})
 			new Vivus('cross', {duration: 50}, );
+
+			muse = s.polygon([
+				0,0,
+				($('#home').width() / 2),($('#home').width() / 2),
+				0,$('#home').width()
+			])
+			muse.attr({
+				fill: '#242424',
+				'fill-opacity': 0
+			});
+
+
+			// muse.attr({ fill: "#000", stroke: "green", 'fill-opacity': 1 })
+
+			function animateStart(){
+				muse.animate({ 
+					fill: "#000", 
+					'fill-opacity': 1 
+				}, 1000)
+			}
+
+			$('polygon').click(function(){
+				animateStart()
+				console.log('click')
+			})
+
+
+
+
+			$('#music').click(function(){
+				// console.log(muse)
+				$('polygon').trigger('click')
+			})
+			
 		})
 	</script>
 
